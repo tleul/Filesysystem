6754d48a6eede857736f68e951557b4e134192de
Tekleselassie Leul <edenlove2019@gmail.com>
2020-10-11 23:58:35 -0700

    services init2

--- services/get.js
+++ services/get.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
+const fs = require('fs');
 const router = express.Router();
-
+const tododata = 'user.json';
 router.get('/', (req, res) => {});
 module.exports = router;
--- services/post.js
+++ services/post.js
@@ -1,6 +1,21 @@
 const express = require('express');
-
+fs = require('fs');
 const router = express.Router();
+const tododata = 'user.json';
+const data = require('../user.json');
+router.post('/', (req, res) => {
+	todo = {
+		id: 1,
+		title: 'first TODO',
+		desc: 'This is the first to do to be tested ',
+	};
 
-router.post('/', (req, res) => {});
+	data.push(todo);
+
+	fs.writeFile(tododata, JSON.stringify(data), function (err) {
+		if (err) return console.log(err);
+		console.log('Hello World > helloworld.txt');
+	});
+	res.json(data);
+});
 module.exports = router;
