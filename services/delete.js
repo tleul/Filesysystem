a628645b39b7eb494756972b93cd61bb63339947
Tekleselassie Leul <edenlove2019@gmail.com>
2020-10-14 09:51:20 -0700

    edit ui

--- services/delete.js
+++ services/delete.js
@@ -4,11 +4,12 @@ const todolist = require('../user.json');
 const filename = 'user.json';
 const router = express.Router();
 
-router.delete('/${id}', (req, res) => {
+router.delete('/:id', (req, res) => {
 	const updated = todolist.filter((todo) => todo.id !== req.params.id);
 
-	fs.writeFile(filename, updated, function writeJson(err, data) {
-		console.log(data);
+	fs.writeFile(filename, JSON.stringify(updated), function (err) {
+		if (err) console.log(err);
 	});
+	return res.json(updated);
 });
 module.exports = router;
